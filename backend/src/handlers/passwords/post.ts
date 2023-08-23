1f8f0489e9076e4d8e9cc3b838a2a0f3b2b41682
Kirill Eremin <kirill.eremins@gmail.com>
2023-08-23 23:45:56 +0300

    task-7: Добавил отправку backup-а в telegram

--- backend/src/handlers/passwords/post.ts
+++ backend/src/handlers/passwords/post.ts
@@ -1,11 +1,15 @@
 import { Request, Response } from "express";
 
-import { writeStore } from "../../store.js";
+import { writeStore, passwordsFilePath } from "../../store.js";
+import { sendTelegramFile } from "../../api/telegram/sendFile.js";
 
 export function postPasswords(req: Request, res: Response) {
   const requestData = req.body.data;
+  const data = JSON.stringify(requestData, null, 4);
 
-  writeStore(JSON.stringify(requestData, null, 4));
+  writeStore(data);
+
+  sendTelegramFile(data);
 
   res.sendStatus(200);
 }
