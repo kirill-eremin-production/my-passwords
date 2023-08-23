aa887f57e38ee1b0bb3784e238390dc21ec9723d
Kirill Eremin <kirill.eremins@gmail.com>
2023-08-23 17:27:29 +0300

    task-7: Реализовал отправку кода в Telegram

---
+++ backend/src/api/telegram/sendMessage.ts
@@ -0,0 +1,18 @@
+import { env } from "../../env.js";
+
+export async function sendTelegramMessage(text: string) {
+  return await fetch(
+    `https://api.telegram.org/bot${env.telegramBotSecret}/sendMessage`,
+    {
+      method: "POST",
+      headers: new Headers({
+        "Content-Type": "application/json",
+      }),
+      body: JSON.stringify({
+        chat_id: env.telegramUserId,
+        parse_mode: "Markdown",
+        text,
+      }),
+    }
+  );
+}
